6b3ac611ece9d0f95428714e5e34b88e34d2d333
z4xanil <metehanbatu67@gmail.com>
2026-01-11 17:33:52 +0300

    feat: enable tawk live chat

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ import Providers from "./providers";
 export const metadata = {
   title: "AceHub",
   description: "AceHub - Premium bot paketleri ve 7/24 destek",
-  metadataBase: new URL("http://localhost:3000"),
+  // metadataBase: new URL("http://localhost:3000"), // BUNU KALDIR (Vercel'de localhost olmaz)
 };
 
 export default function RootLayout({ children }) {
@@ -18,26 +18,23 @@ export default function RootLayout({ children }) {
           <Navbar />
           {children}
           <Footer />
-        </Providers>
 
-        {/* Tawk.to Live Chat */}
-        <Script
-          id="tawk-to"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+          {/* Tawk.to Live Chat (Client-side) */}
+          <Script id="tawk-to" strategy="afterInteractive">
+            {`
               var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
               (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                var s1=document.createElement("script"),
+                    s0=document.getElementsByTagName("script")[0];
                 s1.async=true;
                 s1.src='https://embed.tawk.to/6962bd6438e1ce19860d639f/1jekr7kcm';
                 s1.charset='UTF-8';
                 s1.setAttribute('crossorigin','*');
                 s0.parentNode.insertBefore(s1,s0);
               })();
-            `,
-          }}
-        />
+            `}
+          </Script>
+        </Providers>
       </body>
     </html>
   );
