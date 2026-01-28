4a7903ed24e17145e9141cf51d13b121c7322d82
z4xanil <metehanbatu67@gmail.com>
2026-01-29 02:24:59 +0300

    fix: use ESM next config (mjs) for type=module

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: "export",  // <-- varsa kaldır
-};
-
-module.exports = nextConfig;
---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // şimdilik boş, gerekiyorsa buraya ayar ekleriz
+};
+
+export default nextConfig;
